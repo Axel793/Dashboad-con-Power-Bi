faf3c21f925562ccbdb6c58e51e18b1d328fb887
Axel793 <cofasicarrion@gmail.com>
2025-06-13 00:50:51 -0500

    clarify iframe registry comment

--- flutter_webapp/lib/main.dart
+++ flutter_webapp/lib/main.dart
@@ -1,4 +1,41 @@
 import 'package:flutter/material.dart';
+import 'dart:html' as html;
+import 'dart:ui' as ui;
+
+class PowerBIFrame extends StatelessWidget {
+  const PowerBIFrame({super.key});
+
+  static const _viewType = 'powerbi-frame';
+  static bool _registered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    if (!_registered) {
+      // "platformViewRegistry" solo existe en web; por eso ignoramos
+      // el aviso del analizador de que el prefijo está indefinido.
+      // ignore: undefined_prefixed_name
+      ui.platformViewRegistry.registerViewFactory(
+        _viewType,
+        (int viewId) {
+          final element = html.IFrameElement()
+            ..src =
+                'https://app.powerbi.com/view?r=eyJrIjoiODNmMTc3ZjEtY2EwMC00ZmU3LWIxMDUtNmRkZTIwNmMyMzVlIiwidCI6ImU5NDgwZWI4LTdjZWUtNDJjMi04YzM1LTVkMTIyZWNjNWZkOSIsImMiOjR9'
+            ..style.border = '0'
+            ..style.width = '100%'
+            ..style.height = '100%'
+            ..allowFullscreen = true;
+          return element;
+        },
+      );
+      _registered = true;
+    }
+
+    return const AspectRatio(
+      aspectRatio: 16 / 9,
+      child: HtmlElementView(viewType: _viewType),
+    );
+  }
+}
 
 void main() {
   runApp(const MyApp());
@@ -37,7 +74,7 @@ class DashboardPage extends StatelessWidget {
               style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 16),
-            Text('Aqui se integrarán los indicadores de Power BI Service.'),
+            PowerBIFrame(),
           ],
         ),
       ),
